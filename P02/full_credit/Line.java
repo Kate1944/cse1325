adfb18e9641df3e1feb7d136ce59dc81bdb28942
Kaitlin Martin <kxm1944@mavs.uta.edu>
2023-09-01 21:45:56 -0500

    first commit

---
+++ P02/full_credit/Line.java
@@ -0,0 +1,14 @@
+Public class Line {
+	private double x1; //these are fields
+	private double x2;
+	private double y1;
+	private double y2;
+	private Color color;
+	
+	Public Line (double x1, double x2, double y1, double y2) {
+	this.x1 = x1; //this assigns parameter x1 to field x1
+	this.x2 = x2; //this is a constructor
+	this.y1 = y1;
+	this.y2 = y2;
+	}
+}
