7c925e817c1c505f7599956c772f93eb1919e2f3
Kaitlin Martin <kxm1944@mavs.uta.edu>
2023-09-02 12:05:04 -0500

    tenth commit

--- P02/full_credit/TestLine.java
+++ P02/full_credit/TestLine.java
@@ -1,7 +1,13 @@
 public class TestLine {
 	public static void main (String[] args) {
+
 		Line a = new Line(Color.FUSCHIA, 1, 2, 3, 4);
-		
+		a.length();
+		Line b = new Line(Color.CERULEAN, 5.45, 8.12, 9.78, 2.63);
+		Line c = new Line(Color.SAFFRON, 6, 6, 6, 6);
 		System.out.println(a);
+		System.out.println(a.length());
+		//System.out.println(b + l);
+		//System.out.println(c + l);
 	}
 }
