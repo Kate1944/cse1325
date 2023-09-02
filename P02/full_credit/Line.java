7ee161cdb34957e9a10a9eafe5d5fb34919b8fe6
Kaitlin Martin <kxm1944@mavs.uta.edu>
2023-09-01 23:39:59 -0500

    fifth commit

--- P02/full_credit/Line.java
+++ P02/full_credit/Line.java
@@ -5,20 +5,20 @@ public class Line {
 	private double y2;
 	private Color color;
 	
-	public Line (double x1, double x2, double y1, double y2) {
+	public Line (Color color, double x1, double x2, double y1, double y2) {
 		this.x1 = x1; //this assigns parameter x1 to field x1
 		this.x2 = x2; //this is a constructor
 		this.y1 = y1;
 		this.y2 = y2;
 	}
-	@Override
-	public length() {
+	
+	public double length() {
 		double l = Math.sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)));
-		return l;
+		return l; //this is a method
 	}
 	
-	
+	@Override
 	public String toString() {
-		return color + " (" + x1 + ", " + x2 + ")-(" + y1 + ", " + y2 + ")";
+		return color + " (" + x1 + ", " + x2 + ")-(" + y1 + ", " + y2 + ")" + l;
 	}
 }
--- P02/full_credit/TestLine.java
+++ P02/full_credit/TestLine.java
@@ -1,4 +1,8 @@
-public static void main (String[] args) {
-	Line a = new Line();
+public class TestLine {
+	public static void main (String[] args) {
+		Line a = new Line(1, 2, 3, 4);
+		
+		System.out.println(a + " has length ");
 	
+	}
 }
