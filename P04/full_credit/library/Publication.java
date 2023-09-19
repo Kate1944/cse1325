8d22938a845ced08e2398e7bc467ca3218697724
Kaitlin Martin <kxm1944@mavs.uta.edu>
2023-09-18 23:41:34 -0500

    second commit

--- P04/full_credit/library/Publication.java
+++ P04/full_credit/library/Publication.java
@@ -10,18 +10,19 @@ package library;
 
 import java.time.LocalDate;
 public class Publication {
+    public static final int LOAN = 14; //days until publication is due
     public Publication (String title, String author, int copyright) {
         this.title = title;
         this.author = author;
         this.copyright = copyright;
+        if(copyright < 1900 || copyright > LocalDate.now().getYear()) {
+            throw new IllegalArgumentException("Invalid copyright year: " + copyright);
+        }
     }
-    //TODO: throw IllegalArgumentException
 
     public void checkOut(String patron) {
-        //String patron = loanedTo;
-        LocalDate now = LocalDate.now();
-        LocalDate future = now.plusDays(14);
-        LocalDate dueDate = future;
+        loanedTo = patron;
+        dueDate = LocalDate.now().plusDays(LOAN);
     }
 
     /*public checkIn() {
@@ -29,16 +30,16 @@ public class Publication {
     } */
 
     public String toString() {
-        /*if(loanedTo.equals(NULL)) {
-            return "'" + title + "' by " + author + ", copyright " + copyright;
+        if(loanedTo != null) {
+            return "'" + title + "' by " + author + ", copyright " + copyright + 
+            "\n  ...This book is loaned to " + loanedTo + " until: " + dueDate;
         }
         else {
-            return "'" + title + "' by " + author + ", copyright " 
-            + copyright + "...This book is loaned to " + loanedTo + "until: " + dueDate;
+            return "'" + title + "' by " + author + ", copyright " + copyright;
         }
         
-        */
-        return "\n" + "'" + title + "' by " + author + ", copyright " + copyright + "\n";
+        
+        //return "\n" + "'" + title + "' by " + author + ", copyright " + copyright + "\n";
         
         }
 
--- P04/full_credit/mdi/LibraryManager.java
+++ P04/full_credit/mdi/LibraryManager.java
@@ -16,13 +16,20 @@ public class LibraryManager {
         //adds 3 Publications to the Library
         //asks user to check out a book
         //prints Library to console twice
-
-        Library library = new Library("UTA Library");
-        library.addPublication(new Publication("The Bell Jar", "Sylvia Plath", 1963));
-        library.addPublication(new Publication("Lord of the Flies", "William Golding", 1954));
-        library.addPublication(new Publication("1984", "George Orwell", 1949));
-        //library.checkOut(0, "Bobby");
+        try {
+            Library library = new Library("UTA Library");
+            library.addPublication(new Publication("The Bell Jar", "Sylvia Plath", 1963));
+            library.addPublication(new Publication("Lord of the Flies", "William Golding", 1954));
+            library.addPublication(new Publication("1984", "George Orwell", 1949));
         
-        System.out.println(library);
+            System.out.println(library);
+            int selection = Integer.parseInt(System.console().readLine("\nWhich book would you like to check out?"));
+            String patron = System.console().readLine("\nWhat's your name?");
+            library.checkOut(selection, patron);
+            System.out.println(library);
+        } catch(Exception e){
+            System.err.println("Unable to check out publication\n" + e);
+        }
+        
     }
 }
