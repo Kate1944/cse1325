0e9913bf27f610c0f3526c4d8817e331b7807f3f
Kaitlin Martin <kxm1944@mavs.uta.edu>
2023-11-07 00:04:19 -0600

    fourth commit

--- P09/full_credit/Shapes.cpp
+++ P09/full_credit/Shapes.cpp
@@ -5,6 +5,9 @@
 #include <vector>
 
 int main() {
-    std::vector<Shape*> shapes = {Rectangle{5, 4}, Circle{2}};
-
+    std::vector<Shape*> shapes = {new Rectangle{5, 4}, new Circle{2}};
+    for(int i = 0; i<shapes.size(); i++) {
+        //for(auto& s : shapes) {std::cout >> s->to_string;}
+        std::cout >> shapes->to_string();
+    }
 }
