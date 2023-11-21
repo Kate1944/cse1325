db50f87531d5eb8a5d70f97d2070cea32d431ea2
Kaitlin Martin <kxm1944@mavs.uta.edu>
2023-11-21 02:57:46 -0600

    commit 4

--- P11/full_credit/Index.cpp
+++ P11/full_credit/Index.cpp
@@ -6,13 +6,16 @@
 typedef std::string Word;
 typedef std::set Locations;
 
+
 void add_word(Word word, std::string filename, int line) {
 
 }
 
+typedef std::map<Word, Locations> Index;
 std::ostream& operator<<(std::ostream& ost, const Index& index) {
-    ost <<  location._filename << ' line '
-        <<  location._line;
+    for(const auto& [word, locations] : index) {
+        std::cout << word << "" << locations;
+    }
         
     return ost;
 }
